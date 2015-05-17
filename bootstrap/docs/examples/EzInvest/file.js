6732f8fea8485ee2a97a54cd43e0bb6b7b6adae2
kanke Ishaku <kanke.ishaku@live.com>
2015-05-17 08:07:45 +0100

    first commit

---
+++ bootstrap/docs/examples/EzInvest/file.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+currency=new Array(
+"British Pound",
+"U.S. Dollar",
+"Russian Ruble");
+stocks=new Array("Large-Cap","Mid-Cap"," Small-Cap");
+commodity=new Array("Soft","Hard","Merging");
+hedges=new Array("Long-Short Funds","Market-Neutral Funds","Event-Driven Fund","Macro Funds");
+
+populateSelect();
+
+$(function() {
+
+      $('#category').change(function(){
+        populateSelect();
+    });
+    
+	$('#item').change(function(){
+	
+$('#table').css("display", "block");
+	  });
+}); 
+
+
+function populateSelect(){
+    category=$('#category').val();
+    $('#item').html('');
+    
+       eval(category).forEach(function(t) { 
+            $('#item').append('<option>'+t+'</option>');
+        });
+    }
+ 
+ 
+
+});
+    
+ 
